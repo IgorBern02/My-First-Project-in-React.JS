e6af55ef5a694d53edcfec68ce1fbc05d24370ad
Igor Bernardes <igor.bernardes2@gmail.com>
2024-02-22 14:45:52 -0300

    landing page st

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import logo from "../image/LogoNetflix.png";
+
+function Logo() {
+  return (
+    <div className="Logo">
+      <img src={logo} alt="Logo Netflix" />
+    </div>
+  );
+}
+
+export default Logo;
---
+++ src/components/Title.jsx
@@ -0,0 +1,10 @@
+function Title() {
+  return (
+    <div className="Title">
+      <span>Serie</span>
+      <h1>Stanger Things</h1>
+    </div>
+  );
+}
+
+export default Title;
